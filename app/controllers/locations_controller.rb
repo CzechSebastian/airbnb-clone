ab63193bb2f4d24af801d4b77bb7d60dc6cb153e
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2019-05-21 15:47:14 -0400

    add upload picture to form

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -56,6 +56,10 @@ class LocationsController < ApplicationController
   private
 
   def location_params
-    params.require(:location).permit(:name, :address, :category, :description, :price)
+    params.require(:location).permit(:name, :address, :category, :description, :price, :photo)
   end
+
+  def article_params
+  params.require(:article).permit(:title, :body, :photo)
+end
 end
