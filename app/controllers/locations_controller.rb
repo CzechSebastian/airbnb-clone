9bd4df9824840734908ff25507dd317605cd1d86
Sebastian Czech <czechsebastian@icloud.com>
2019-05-22 12:42:49 -0400

    searchbar working

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,7 +5,13 @@ class LocationsController < ApplicationController
 
   def index
     @locations = policy_scope(Location)
+    if params[:search]
+      @locations = Location.search(params[:search]).order("created_at DESC")
+    else
+      @location = Location.all.order('created_at DESC')
+    end
   end
+
 
   def dashboard
   end
