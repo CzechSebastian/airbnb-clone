fb4c09ff11263b01a808665aaf349623667c4d8b
Sebastian Czech <czechsebastian@icloud.com>
2019-05-24 11:45:39 -0400

    implement sweet alert

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -1,29 +1,37 @@
 import swal from 'sweetalert';
 
-
 const initSweetalert = () => {
-  const options = {
-    title: "Are you sure?",
-    text: "Once deleted, you will not be able to recover this item!",
-    icon: "warning",
-    buttons: true,
-    dangerMode: true,
-  };
 
-  const swalButtons = document.querySelectorAll('.delete-link');
-
-  if(swalButtons) {
-    swalButtons.forEach((button) => {
-      button.addEventListener('click', (event) => {
-        console.log("clicked")
-          const link = event.currentTarget;
-          swal(options).then((value) => {
-            if (value) { link.previousElementSibling.click(); };
-          })
-        });
-      }
-    );
+  const applySweetalert = (element, options = {}, callback = () => {}) => {
+    element.addEventListener('click', () => {
+      swal(options).then(callback); // <-- add the `.then(callback)`
+    });
   }
+
+
+  const deleteButtons = document.querySelectorAll('.delete-btn');
+
+  if(deleteButtons) {
+    const options = {
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this item!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    };
+
+    deleteButtons.forEach((button) => {
+      const listingId = button.dataset.listingId
+      const callback = (value) => {
+        if (value) {
+            const link = document.querySelector(`a[data-listing-id='${listingId}']`);
+            link.click();
+        }
+      }
+      applySweetalert(button, options, callback)
+    })
+  }
+
 }
 
 export { initSweetalert };
