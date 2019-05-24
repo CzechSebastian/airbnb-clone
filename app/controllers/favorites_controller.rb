b1de8ea7f52a2f99e5a1b3bfc0c527c22c60c2ae
Benjamin Domingue <benjamin.domingue1991@gmail.com>
2019-05-24 13:06:41 -0400

    add backend

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,12 @@
 class FavoritesController < ApplicationController
 
-	def create
-		@location = Location.find(params[:location_id])
-		@favorite = Favorite.new
-		@favorite.user = current_user
-		@favorite.location = @location
-		@favorite.save
-		authorize @favorite
+def create
+	 	 @location = Location.find(params[:location_id])
+	 	 @favorite = Favorite.new
+	 	 @favorite.user = current_user
+	 	 @favorite.location = @location
+		 @favorite.save
+		 authorize @favorite
 	end
-
 
 end
